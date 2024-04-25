ca3f086e6539716e28ca103c28a4f855f8643ffb
Chris <chris284891@gmail.com>
2024-04-25 15:01:45 -0400

    practicing

---
+++ demo.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Hello GFG </h1>
+ 
+ <?php
+$b = array();
+$b[] = 'a';
+$b[] = 'b';
+$b[] = 'c';
+var_export($b);
+ ?>
+</body>
+</html>
